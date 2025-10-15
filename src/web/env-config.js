ac437d98cbca5bbc71b58c1e285b4aee2ae3407a
ekinbulut <ekinbulut@gmail.com>
2025-10-15 13:26:48 +0300

    Update environment configuration for local development URLs

--- src/web/env-config.js
+++ src/web/env-config.js
@@ -2,8 +2,8 @@
 // This file is used when running outside Docker
 if (typeof window !== 'undefined' && !window.ENV) {
     window.ENV = {
-        AUTH_BASE_URL: 'http://192.168.1.112:8082',
-        MOVIES_BASE_URL: 'http://192.168.1.112:8080',
+        AUTH_BASE_URL: 'http://localhost:5188',
+        MOVIES_BASE_URL: 'http://localhost:5154',
         JELLYFIN_BASE_URL: 'http://192.168.1.112:8096',
         JELLYFIN_ACCESS_TOKEN: ''
     };
